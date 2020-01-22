096ff96f13945959bf33ddb9ca6d204626db033b
Luis Lucana <luislucana@gmail.com>
2020-01-21 23:41:50 -0300

    Criação dos arquivos iniciais.

---
+++ src/main/java/br/com/exemplo/demofileapi/DemoFileApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.demofileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFileApiApplication.class, args);
+	}
+
+}
